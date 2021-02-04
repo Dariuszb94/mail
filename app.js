05f486574eee06551180b8bacc61e0321872ae80
dariuszb <dariusz.berer@gmail.com>
2021-02-04 21:37:26 +0100

    class read fix

--- app.js
+++ app.js
@@ -2,23 +2,25 @@ import { route } from "./router";
 import { home } from "./templates/index.js";
 import { example1 } from "./templates/index.js";
 import { example2 } from "./templates/index.js";
-console.log(home);
 route("/", home.innerHTML, function () {
   //route("/", "home", function () {
   this.where = "here";
 });
 
-// route("/ex1", example1.innerHTML, function () {
-route("/ex1", "example1", function () {
+route("/ex1", example1.innerHTML, function () {
+  //route("/ex1", "example1", function () {
   this.title = "Example 1";
 });
 
-// route("/ex2", example2.innerHTML, function () {
-route("/ex2", "example2", function () {
+route("/ex2", example2.innerHTML, function () {
+  //route("/ex2", "example2", function () {
   this.title = "Example 2";
   this.counter = 0;
-  this.$on(".my-button", "click", () => {
+  this.$on("button", "click", () => {
     this.counter += 1;
+    console.log("AAA");
+    console.log(this.counter);
+
     this.$refresh();
   });
 });
--- templates/index.js
+++ templates/index.js
@@ -1,15 +1,16 @@
 let preHome = document.createElement("script");
 preHome.setAttribute("type", "text/html");
-preHome.innerHTML = "<h1>Start <%= where %>!</h1>";
+preHome.innerHTML = "<h1>Start1 <%= where %>!</h1>";
 export const home = preHome;
-
+console.log(home);
 let preExample1 = document.createElement("script");
 preExample1.setAttribute("type", "text/html");
 preExample1.innerHTML =
-  "<h1><%= title %></h1><button class='my-button'>Click me <%= counter %></button>";
+  "<h1><%= title %></h1><% for (let i of title) { %><span><%= i %></span><% } %>";
 export const example1 = preExample1;
 
 let preExample2 = document.createElement("script");
 preExample2.setAttribute("type", "text/html");
-preExample2.innerHTML = "<h1>Start <%= where %>!</h1>";
+preExample2.innerHTML =
+  '<h1><%= title %></h1><button class="my-button">Click me <%= counter %></button>';
 export const example2 = preExample2;
